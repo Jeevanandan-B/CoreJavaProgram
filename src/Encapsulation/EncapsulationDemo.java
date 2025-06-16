9090df65c1d706bc6c9fafbea854014806a5386a
Jeevanandan-B <jeevabass04@gmail.com>
2025-06-16 21:59:38 +0530

    Today I worked on Encapsulation using Oops concepts on java program

---
+++ src/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package Encapsulation;
+
+public class EncapsulationDemo {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(23);
+		obj.setName("Geosha");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
