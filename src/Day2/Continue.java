9c212786cea6bf0133291f3cb4e0b5d6181f62c6
Jeevanandan-B <jeevabass04@gmail.com>
2025-06-16 21:08:01 +0530

    Today I worked Continue on java program

---
+++ src/Day2/Continue.java
@@ -0,0 +1,15 @@
+
+package Day2;
+
+public class Continue {
+	public static void main(String[] args) {
+		for (int i = 1; i < 15; i++)
+		{
+		
+		if (i%2 != 0)
+		continue;
+		
+		System.out.print(i + " ");
+		}
+	}
+}
