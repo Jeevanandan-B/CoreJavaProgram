112159bc5702e846268d3192dab6989a0e82fdbe
Jeevanandan-B <jeevabass04@gmail.com>
2025-06-16 21:05:11 +0530

    Today I worked Char on java program

---
+++ src/Day2/Char.java
@@ -0,0 +1,24 @@
+package Day2;
+
+public class Char {
+
+	public static void main(String[] args) {
+		
+				char ch = 'c';
+				System.out.println(ch);
+
+				char ch1 = 80;
+				System.out.println(ch1);
+
+				char var1 = '\u00B6';
+				System.out.println(var1);
+
+				char var2 = '\u20AC';
+				System.out.println(var2);
+
+				int a = 'B';
+				System.out.println(a);
+
+	}
+
+}
