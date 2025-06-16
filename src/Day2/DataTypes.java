b546a3234b5cc6740b1f88bd3fb1210dd3595ce7
Jeevanandan-B <jeevabass04@gmail.com>
2025-06-16 21:10:06 +0530

    Today I worked in Data Types on java program

---
+++ src/Day2/DataTypes.java
@@ -0,0 +1,22 @@
+package Day2;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int value1 = 7 / 3; //integer division
+		float value2 = 100 / 51;
+		double value3 = 115d / 8d;
+		float value4 = 10/5.5f; //float division
+		System.out.println("value 1=" + value1); 
+		System.out.println("value 2=" + value2);
+		System.out.println("value 3=" + value3);
+		System.out.println("value 4=" + value4);
+		
+		
+		int marker = 450;
+		//assigning expression to variable 
+		double percentage = marker * 0.46f;
+		System.out.println("percentage :" + percentage);
+	}
+
+}
