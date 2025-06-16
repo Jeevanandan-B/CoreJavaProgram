0c1c2ba22ff922b998775e177db998798fe31616
Jeevanandan-B <jeevabass04@gmail.com>
2025-06-16 21:19:59 +0530

    Today I worked For loop on java program

---
+++ src/Day2/ForLoop.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class ForLoop {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=100 ; i++)   
+		//for(int i = 1000; i<=1 ; i--)
+		{	
+			//System.out.print(" i:");
+			System.out.println(i);
+		}
+	}
+}
